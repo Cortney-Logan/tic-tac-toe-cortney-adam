2a6eb24a5524981e3225c4a5ecf1f43fb1576f56
Cortney-Logan <logan.cort@gmail.com)>
2021-02-18 17:03:05 -0500

    adding comments

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ start.addEventListener("click", () => {
 for (let elements of cells) {
   // when clicked the textContent of the cell will change to X or O depending on the currentPlayer.  The current player is then changed
   elements.addEventListener("click", (event) => {
-    //guard clause to prevent a previously clicked cell from being clicked again. If it has already been clicked the player is alerted to pick an empty cell
+    //guard clause to prevent a previously clicked cell from being clicked again. If it has already been clicked the player is alerted to pick an empty cell.  Otherwise it marks the cell appropriately
     if (event.target.textContent === "") {
       if (currentPlayer === "X") {
         event.target.textContent = "X";
@@ -26,12 +26,15 @@ for (let elements of cells) {
       }
     } else alert("Please select an empty cell.");
 
-    //check for win & if there's no win it changes the current player
+    //check for win condition by calling win() function
+    //if win returns true, the current player is declared the winner & the start button is re-enabled
     if (win()) {
       playerStatus.textContent = `Player ${currentPlayer} Won`;
       start.disabled = false;
       cells.children.textContent = "";
-    } else {
+    }
+    // if there's no win it changes the current player changes and game alerts that it is the next player's turn
+    else {
       if (currentPlayer === "X") {
         currentPlayer = "O";
       } else {
@@ -42,6 +45,7 @@ for (let elements of cells) {
   });
 }
 
+//checks for win condition - if a condition is met it returns true, otherwise is undefined (which evaluates to false)
 function win() {
   if (
     cells[0].textContent === cells[3].textContent &&
