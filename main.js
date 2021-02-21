e91b64b8f0b1463da4477490fbb741b7efb9dad3
Cortney-Logan <logan.cort@gmail.com)>
2021-02-21 13:46:38 -0500

    adding user name choose functionality and update win condition

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//-------------------- HTML Variables --------------------//
 // declare js variable targeting start button in game
 let start = document.getElementById("start");
 // declare js variable targeting playerstatus area in game
@@ -8,11 +9,25 @@ let cells = Array.from(document.getElementsByClassName("cell"));
 let hourTimer = document.getElementById("hours");
 let minuteTimer = document.getElementById("minutes");
 let secondTimer = document.getElementById("seconds");
-// declare variable to keep track of current player
-let currentPlayer = "X";
+//declares variables for playervplayer and playervcomputer buttons
 let playerVplayer = document.getElementById("player-vs-player");
 let playerVcomputer = document.getElementById("player-vs-computer");
-let gameType = "";
+//declares varaibles for choosing player x and o names
+let setPlayerXName = document.getElementById("set-player-X-name");
+let setPlayerOName = document.getElementById("set-player-O-name");
+//declares variables to keep track of playerX and playerO names displayed in game
+let playerXNameDisplay = document.getElementById("player-X-name");
+let playerONameDisplay = document.getElementById("player-O-name");
+let chooseNamesForms = document.getElementsByClassName("choose-name");
+
+//-------------------- Native JS Variables --------------------//
+//variables for player x and o names in game - set as default values
+let playerXName = "Player X";
+let playerOName = "Player O";
+
+//declares variables to keep track of current player and current player name
+let currentPlayer = "";
+let currentPlayerName = "";
 
 //declares hour, minute, and second counters for the game timer
 let hours = 0;
@@ -26,13 +41,15 @@ let timer;
 start.disabled = true;
 //playerStatus.textContent = "";
 
+//-------------------- Event Listeners --------------------//
 playerVplayer.addEventListener("click", () => {
   start.disabled = false;
   playerVplayer.disabled = true;
   playerVcomputer.disabled = true;
   gameType = "pVp";
   currentPlayer = "X";
-  playerStatus.textContent = "";
+  //allows users to choose their own names
+  chooseNames();
 });
 
 playerVcomputer.addEventListener("click", () => {
@@ -42,10 +59,16 @@ playerVcomputer.addEventListener("click", () => {
   currentPlayer = "X";
   playerStatus.textContent = "";
   gameType = "pVc";
+  playerXNameDisplay.textContent = "Player X"
+  playerONameDisplay.textContent = "Player O"
 });
 
 //adds event listener to start button - when clicked the button is disabled and game play can begin
 start.addEventListener("click", () => {
+  //removes option to see choose name forms
+  for (let form of chooseNamesForms) {
+    form.style.visibility = "hidden";
+  }
   //clears the game timer to 0
   if (timer) {
     clearInterval(timer);
@@ -60,13 +83,13 @@ start.addEventListener("click", () => {
   //disables start button
   start.disabled = true;
   // player status shows "player x's turn"
-  playerStatus.textContent = "Player X's Turn";
+  playerStatus.textContent = `${playerXName}'s Turn`;
 
   //runs through each cell
   for (let elements of cells) {
     //clear the board for a new game
     elements.textContent = "";
-    elements.style.textDecoration = "none";
+    elements.style.backgroundColor = "white";
 
     if (gameType === "pVp") {
       //adds event listener to each cell to allow for game play
@@ -74,6 +97,49 @@ start.addEventListener("click", () => {
     } else elements.addEventListener("click", playComputerGame);
   }
 });
+
+
+//-------------------- Functions --------------------//
+
+//accepts user name and changes value
+function chooseNames() {
+  //prompts users to enter name
+  playerStatus.textContent = "Choose Your Names";
+  //makes choose-names visible to allow user to submit name choice
+  for (let form of chooseNamesForms) {
+    form.style.visibility = "visible";
+  }
+  //adds even listener to player X name choice
+  setPlayerXName.addEventListener("submit", (evt) => {
+    //prevents default form action of reset
+    evt.preventDefault();
+    //changes player X name to value submitted in player X form
+    playerXName = document.getElementById("playerX").value;
+    //if user does not submit anything - use default name
+    if (playerXName === "") {
+      playerXName = "Player X";
+    }
+    //changes display for Player X to show chosen name
+    playerXNameDisplay.textContent = playerXName;
+    //resets the player X name form
+    setPlayerXName.reset();
+  });
+  setPlayerOName.addEventListener("submit", (evt) => {
+    //prevents default form action of reset
+    evt.preventDefault();
+    //changes player X name to value submitted in player O form
+    playerOName = document.getElementById("playerO").value;
+    //if user does not submit anything - use default name
+    if (playerOName === "") {
+      playerOName = "Player O";
+    }
+    //changes display for Player O to show chosen name
+    playerONameDisplay.textContent = playerOName;
+    //resets the player O name form
+    setPlayerOName.reset();
+  });
+}
+
 
 function playGame(event) {
   //guard clause to prevent a previously clicked cell from being clicked again. If it has already been clicked the player is alerted to pick an empty cell.  Otherwise it marks the cell appropriately
@@ -89,7 +155,7 @@ function playGame(event) {
   //check for win condition by calling win() function
   //if win returns true, the current player is declared the winner, the start button is re-enabled and playGame click listener is removed from all cells
   if (win()) {
-    playerStatus.textContent = `Player ${currentPlayer} Won`;
+    playerStatus.textContent = `${currentPlayerName} Won!`;
     start.disabled = true;
     playerVplayer.disabled = false;
     playerVcomputer.disabled = false;
@@ -107,10 +173,12 @@ function playGame(event) {
   else {
     if (currentPlayer === "X") {
       currentPlayer = "O";
+      currentPlayerName = playerOName;
     } else {
       currentPlayer = "X";
+      currentPlayerName = playerXName;
     }
-    playerStatus.textContent = `Player ${currentPlayer}'s Turn`;
+    playerStatus.textContent = `${currentPlayerName}'s Turn`;
   }
 }
 
@@ -119,8 +187,7 @@ function playComputerGame(event) {
     // when the textContent of the cell will change to X or O depending on the currentPlayer.  The current player is then changed
     if (currentPlayer === "X") {
       event.target.textContent = "X";
-      
-    } else elements.removeEventListener("click", playComputerGame)
+    } else elements.removeEventListener("click", playComputerGame);
 
     let randomGuess = Math.floor(Math.random() * 8);
     console.log(randomGuess);
@@ -129,7 +196,7 @@ function playComputerGame(event) {
     }
     cells[randomGuess].textContent = "O";
     elements.addEventListener("click", playComputerGame);
-  }else alert("Please select an empty cell.");
+  } else alert("Please select an empty cell.");
   if (win()) {
     playerStatus.textContent = `Player ${currentPlayer} Won`;
     start.disabled = true;
@@ -140,14 +207,17 @@ function playComputerGame(event) {
       elements.removeEventListener("click", playComputerGame);
     }
     clearInterval(timer);
-  } else {
-    if (currentPlayer === "X") {
-      currentPlayer = "O";
-    } else {
-      currentPlayer = "X";
-    }
-    playerStatus.textContent = `Player ${currentPlayer}'s Turn`;
   }
+    else {
+      if (currentPlayer === "X") {
+        currentPlayer = "O";
+        currentPlayerName = playerOName;
+      } else {
+        currentPlayer = "X";
+        currentPlayerName = playerXName;
+      }
+      playerStatus.textContent = `${currentPlayerName}'s Turn`;
+    }
 }
 //checks for win condition - if a condition is met it returns true, otherwise is undefined (which evaluates to false)
 function win() {
@@ -157,7 +227,9 @@ function win() {
     cells[3].textContent === cells[6].textContent &&
     cells[0].textContent !== ""
   ) {
-    cells[0].style.textDecoration = "line-through";
+    cells[0].style.backgroundColor = "lightblue";
+    cells[3].style.backgroundColor = "lightblue";
+    cells[6].style.backgroundColor = "lightblue";
     return true;
   }
   //checks win condition for column 2
@@ -166,7 +238,9 @@ function win() {
     cells[4].textContent === cells[7].textContent &&
     cells[1].textContent !== ""
   ) {
-    cells[1].style.textDecoration = "line-through";
+    cells[1].style.backgroundColor = "lightblue";
+    cells[4].style.backgroundColor = "lightblue";
+    cells[7].style.backgroundColor = "lightblue";
     return true;
   }
   //checks win condition for column 3
@@ -175,7 +249,9 @@ function win() {
     cells[5].textContent === cells[8].textContent &&
     cells[2].textContent !== ""
   ) {
-    cells[2].style.textDecoration = "line-through";
+    cells[2].style.backgroundColor = "lightblue";
+    cells[5].style.backgroundColor = "lightblue";
+    cells[8].style.backgroundColor = "lightblue";
     return true;
   }
   //checks win condition for row 1
@@ -184,7 +260,9 @@ function win() {
     cells[1].textContent === cells[2].textContent &&
     cells[0].textContent !== ""
   ) {
-    cells[0].style.textDecoration = "line-through";
+    cells[1].style.backgroundColor = "lightblue";
+    cells[1].style.backgroundColor = "lightblue";
+    cells[2].style.backgroundColor = "lightblue";
     return true;
   }
   //checks win condition for row 2
@@ -193,7 +271,9 @@ function win() {
     cells[4].textContent === cells[5].textContent &&
     cells[3].textContent !== ""
   ) {
-    cells[3].style.textDecoration = "line-through";
+    cells[3].style.backgroundColor = "lightblue";
+    cells[4].style.backgroundColor = "lightblue";
+    cells[5].style.backgroundColor = "lightblue";
     return true;
   }
   //checks win condition for row 3
@@ -202,7 +282,9 @@ function win() {
     cells[7].textContent === cells[8].textContent &&
     cells[6].textContent !== ""
   ) {
-    cells[6].style.textDecoration = "line-through";
+    cells[6].style.backgroundColor = "lightblue";
+    cells[7].style.backgroundColor = "lightblue";
+    cells[8].style.backgroundColor = "lightblue";
     return true;
   }
   //checks win condition for upper left to lower right diagonal
@@ -211,7 +293,9 @@ function win() {
     cells[4].textContent === cells[8].textContent &&
     cells[0].textContent !== ""
   ) {
-    cells[0].style.textDecoration = "line-through";
+    cells[0].style.backgroundColor = "lightblue";
+    cells[4].style.backgroundColor = "lightblue";
+    cells[8].style.backgroundColor = "lightblue";
     return true;
   }
   //checks win condition for upper right to lower left diagonal
@@ -220,11 +304,12 @@ function win() {
     cells[4].textContent === cells[6].textContent &&
     cells[2].textContent !== ""
   ) {
-    cells[2].style.textDecoration = "line-through";
+    cells[2].style.backgroundColor = "lightblue";
+    cells[4].style.backgroundColor = "lightblue";
+    cells[6].style.backgroundColor = "lightblue";
     return true;
   }
 }
-
 //setTimer function prints the updated time to the game board with correct formatting
 function setTimer() {
   //seconds += 1;
